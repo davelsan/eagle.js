642009045c8a1ae2ca936fa305658055014963ac
David Velasco <davvelsan@gmail.com>
2020-07-06 17:52:05 +0200

    chore: update eslint deps, pnpm support, lint fixes

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,14 @@ module.exports = function (api) {
   if (api.env('test')) {
     return {
       presets: [
-        ['@babel/preset-env', {targets: {node: 'current'}}]
+        ['@babel/preset-env', { targets: { node: 'current' } }]
       ]
     }
   }
 
   return {
     presets: [
-      ['@babel/preset-env', {'modules': false}]
+      ['@babel/preset-env', { modules: false }]
     ],
     plugins: ['@babel/plugin-transform-runtime'],
     comments: false
